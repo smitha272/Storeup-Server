56249d7bb37a68ba22a62f7bacaec65a23997164
LAPTOP-H96I75IS\Krishna.R.K <krishna.rattihallikantharaj@sjsu.edu>
2017-12-02 16:15:57 -0800

    no message

--- routes/coupons.js
+++ routes/coupons.js
@@ -8,7 +8,7 @@ router.get('/getCoupon', function(req, res, next) {
     var email    = req.query.email;
     console.log(email);
 
-    db.query('select store_name,user_name, COUNT(*) as Count from receipt_details where user_name = ? group by store_name',[email], function(err, rows, fields) {
+    db.query('select store_name,user_name,store_address, latitude, longitude,distance_traveled_by_user, COUNT(*) as Count from receipt_details where user_name = ? group by store_name',[email], function(err, rows, fields) {
         if (err) throw err;
 
         if (rows.length > 0) {
